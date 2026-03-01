c19e3064693553669ea63846bdf94042d489c603
Hyounjun Chang <hyounjun.chang@colorado.edu>
2026-02-28 23:12:42 -0700

    Fix errors with building, change a7 commit hash

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -7,7 +7,7 @@
 
 # Claude chat history: https://claude.ai/chat/52a7fcbb-e345-4cd7-ab27-c090f0d200f7
 
-LDD_VERSION = ee4de8b03cc26f91c6ac853865bfbea7f6f1bd23
+LDD_VERSION = f9c4647abb9e39617e00ed3c860e0953456efada
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -17,12 +17,13 @@ LDD_GIT_SUBMODULES = YES
 
 # Makefile in directory builds all submodules
 define LDD_BUILD_CMDS
-	$(MAKE) -C $(LINUX_DIR) M=$(@D) $(LINUX_MAKE_FLAGS)
+	$(MAKE) -C $(LINUX_DIR) M=$(@D)/misc-modules EXTRA_CFLAGS="-I$(@D)/include" $(LINUX_MAKE_FLAGS)
+	$(MAKE) -C $(LINUX_DIR) M=$(@D)/scull EXTRA_CFLAGS="-I$(@D)/include" $(LINUX_MAKE_FLAGS)
 endef
 
 define LDD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules
-	find $(@D) -name "*.ko" | xargs -I{} $(INSTALL) -m 755 {} $(TARGET_DIR)/lib/modules
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/a7
+	find $(@D) -name "*.ko" | xargs -I{} $(INSTALL) -m 755 {} $(TARGET_DIR)/lib/modules/a7
 endef
 
 $(eval $(kernel-module))
